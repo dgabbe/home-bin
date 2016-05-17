60b2dc7a10357b3444407f96a9fd4b4bf713b621
David G <dgabbe@acm.org>
2016-05-17 11:32:14 -0400

    Added, but not working the way expected.

--- os-x-config/os-x-security-tweaks.sh
+++ os-x-config/os-x-security-tweaks.sh
@@ -3,6 +3,9 @@
 # Disable Bonjour multicast advertisements.  See https://www.trustwave.com/Resources/SpiderLabs-Blog/mDNS---Telling-the-world-about-you-(and-your-device)/
 sudo defaults write /Library/Preferences/com.apple.mDNSResponder.plist NoMulticastAdvertisements -bool YES
 
+# After running this script, open System Preferences->Spotlight to check that the 
+# changes are what you intended.
+
 # Customize to your preferences!
 defaults write com.apple.spotlight orderedItems -array \
   '{"enabled" = 1;"name" = "APPLICATIONS";}' \
@@ -10,7 +13,7 @@ defaults write com.apple.spotlight orderedItems -array \
   '{"enabled" = 1;"name" = "DIRECTORIES";}' \
   '{"enabled" = 1;"name" = "PDF";}' \
   '{"enabled" = 1;"name" = "FONTS";}' \
-  '{"enabled" = 0;"name" = "DOCUMENTS";}' \
+  '{"enabled" = 1;"name" = "DOCUMENTS";}' \
   '{"enabled" = 0;"name" = "MESSAGES";}' \
   '{"enabled" = 0;"name" = "CONTACT";}' \
   '{"enabled" = 0;"name" = "EVENT_TODO";}' \
@@ -18,8 +21,8 @@ defaults write com.apple.spotlight orderedItems -array \
   '{"enabled" = 0;"name" = "BOOKMARKS";}' \
   '{"enabled" = 0;"name" = "MUSIC";}' \
   '{"enabled" = 0;"name" = "MOVIES";}' \
-  '{"enabled" = 0;"name" = "PRESENTATIONS";}' \
-  '{"enabled" = 0;"name" = "SPREADSHEETS";}' \
+  '{"enabled" = 1;"name" = "PRESENTATIONS";}' \
+  '{"enabled" = 1;"name" = "SPREADSHEETS";}' \
   '{"enabled" = 0;"name" = "SOURCE";}' \
   '{"enabled" = 0;"name" = "MENU_DEFINITION";}' \
   '{"enabled" = 0;"name" = "MENU_OTHER";}' \
